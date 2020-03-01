304bf34e53397e9675b460a5975e5a8cf83f301b
Victoria <Bernard.victoria22@gmail.com>
2020-03-01 16:16:15 -0600

    Bridesmaid profile

--- src/profiles.tsx
+++ src/profiles.tsx
@@ -3,7 +3,13 @@ import ericPhotoUrl from './img/eric.jpg'
 import krisPhotoUrl from './img/kris.png'
 import brianPhotoUrl from './img/brian.png'
 import paulPhotoUrl from './img/paul.jpg'
-
+import lauraPhotoUrl from './img/wedding_party/Laura.jpg'
+import amyPhotoUrl from './img/wedding_party/Amy.jpg'
+import guliettePhotoUrl from './img/wedding_party/Guliette.jpg'
+import sofiaPhotoUrl from './img/wedding_party/Sofia.jpg'
+import christinaPhotoUrl from './img/wedding_party/Christina.jpg'
+import amandaPhotoUrl from './img/wedding_party/Amanda.jpg'
+import tanyaPhotoUrl from './img/wedding_party/Tanya.jpg'
 export const profiles = [
   {
     name: 'Eric Fawcett',
@@ -205,6 +211,70 @@ export const profiles = [
           called Allerion. He’s the reason why I got into music and still play
           guitar today. Thanks for giving me a life-long hobby, brother.
         </p>
+      </>
+    ),
+  },
+  {
+    name: 'Laura Olivares',
+    photoUrl: lauraPhotoUrl,
+    bio: (
+      <>
+        <p>Victoria’s best friend. They met at UCLA during sorority rush  - though the sorority didn’t work out for V 😐-  she knew she wanted to be Laura’s sister for life. V stalked Laura’s first waitressing job at O’haras where they ended up working together. Shortly after, they became best friends /  roommates / soulmates in Westwood and again in Santa Monica. Laura gives unconditional love even after trauma, vast amounts of lost shoes, and beautifully ruined borrowed outfits. Laura has visited John and V in Austin more than anyone else. Laura was also the very first of the wedding party to meet our first adopted child - Cersei. No one can make us laugh or love like Laura.</p>
+      </>
+    ),
+  },
+  {
+    name: 'Amy Razouk',
+    photoUrl: amyPhotoUrl,
+    bio: (
+      <>
+        <p>Amy is Victoria’s oldest sister and has been a very “non-impartial” surrogate mother to her whole life. From constant hip-riding, diaper changes, and prom night party chaperoning their love has never wavered. Amy couldn’t be prouder of the little ‘puker’ she ‘raised.’ Bragging everywhere she goes about her little masterpiece who has been so studious, successful, self-aware, and hardworking has felt almost surreal. Although on the outside she might seem to be taking all credit, she knows that the thing she is most proud is that Victoria really started as this ‘little person’ that did it all on her own and finding a match like John.
+</p>
+      </>
+    ),
+  },
+  {
+    name: 'Guliette Bernard',
+    photoUrl: guliettePhotoUrl,
+    bio: (
+      <>
+        <p>There is no one with quite as many funny stories as Guliette. The slightly older middle sister to little miss Pookieness22. Best friends from the days of Barbie Dolls, and putting fire ant piles down each others pants. They know how to keep each other laughing, and right out of the midst of trouble. The saying two peas in a pod used to make us bashful, but we now find it endearing. The love we have for one another will continue to thrive far beyond the childhood we once shared in the backwoods of rural Texas, and in the buzzing streets of Silicon Valley.</p>
+      </>
+    ),
+  },
+  {
+    name: 'Sofia Knöppel',
+    photoUrl: sofiaPhotoUrl,
+    bio: (
+      <>
+        <p>At UCLA Sofia and Victoria became friends through a love of venturing from Westwood into LA’s party capital - Hollywood. What started out as a relationship based on partying quickly evolved as they recognized a shared drive for fitness and career goals. Sofia made Victoria family by introducing her to Sofia’s phenomenal sister, Christina, where they were all three roommates. </p>
+      </>
+    ),
+  },
+  {
+    name: 'Christina Sunseri',
+    photoUrl: christinaPhotoUrl,
+    bio: (
+      <>
+        <p>Since middle school Christina and I have been through so much together: car accidents, diagnoses, abandoned Christmases and not to mention all the trouble we’ve gotten each other in. She’s always been there to adopt me, make me laugh and put my life back together no matter what. I knew I wanted her in my wedding since the moment she exited the Valley Christian uniform one for a Castilero one. </p>
+      </>
+    ),
+  },
+  {
+    name: 'Amanda Trillo',
+    photoUrl: amandaPhotoUrl,
+    bio: (
+      <>
+        <p>Amanda we call Trillo because though her name is shared by many, she is a true one of a kind. Pooks has known Amanda since middle school and got to really be friends on the cheer squad at Pioneer High. They were roommates in LA where they turned into true life long friends. </p>
+      </>
+    ),
+  },
+  {
+    name: 'Tanya Faris',
+    photoUrl: tanyaPhotoUrl,
+    bio: (
+      <>
+        <p>V’s absolute best friend is Austin, even their pups Graham and Mister are BFFs. We love Tanya because she motivates us to get up on the microphone not just on karaoke night’s but every night of the week. Victoria’s beauty artist turned a dull burnette into a brilliant blonde.  She shares a deep relationship with Jesus and is one beautifully hilarious being. </p>
       </>
     ),
   },
