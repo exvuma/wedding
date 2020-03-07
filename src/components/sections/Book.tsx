3ac397be2d0607607e2bd2b08e6c8744e8f4b498
Victoria <Bernard.victoria22@gmail.com>
2020-03-07 16:21:25 -0600

    Content structure for John to work with easier

--- src/components/sections/Book.tsx
+++ src/components/sections/Book.tsx
@@ -3,23 +3,25 @@ export const Book: React.FC = () => (
   <ContainerSmall>
     <div>
       <p>
-        John and Victoria are getting married. This is not the official
-        invitation, but here are some things you should know:
+        John and Victoria are getting married. We want you there to share our
+        special day more than any gift on a registary. We are get
       </p>
-      <ul>
-        <li>You need a passport</li>
-        <li>
-          Plan to stay at least 3 nights in the window November 18th - 22nd
-        </li>
-        <li>
-          <strong>Do not</strong> book a hotel, Airbnb, or any other lodging
-          just yet
-          <ul>
-            <li>We will send you information regarding the room block</li>
-          </ul>
-          <li>You may buy flights now</li>
-        </li>
-      </ul>
+      We’re getting married at the beautiful Hyatt Ziva in Cancun Mexico. We
+      want you there to share our special day with us more than any gift on a
+      registry. Book your room on the hotel’s room block we have set up as soon
+      as possible. To work with your budget, the travel agency allows interest
+      free payments. To book your hotel reservation start out by visiting our
+      travel agent’s [booking site](). You’ll find a lot more [Book hotel
+      here](https://www.destinationweddings.com/Weddings/Guests/AttendaWedding.aspx?dw_weddingcode=MARRYTHEJOHN&dw_bname=Bernard)
+      Wedding couple’s last name: Fawcett Wedding code: MARRYTHEJOHN Our number
+      one goal is to get you, our guests, there and meet one another. Please
+      don’t hestiate to ask for help for us to get you there. For any inquiries
+      about the room block or transportation contact our amazing travel agent,
+      Jessica Bull. If you get stuck any step of the way aside from room booking
+      contact us. Victoria - (408)296-2467 or John (469) 587-5077 TLDR; Book
+      your hotel book now for at least the minimum days you’d like to stay. You
+      can always add days to your reservations later. Book your flight on your
+      favorite airline (we recommend Southwest ) whatever time works.
     </div>
   </ContainerSmall>
 )
--- src/components/sections/Faq.tsx
+++ src/components/sections/Faq.tsx
@@ -4,24 +4,32 @@ export const Faq: React.FC = () => (
     <div>
       <ul>
         <li>
-          <strong>
-            What day should I arrive? Leave? The wedding party will arrive
-          </strong>
+          <strong>What day should I arrive? Leave?</strong>
           <br />
-          Wednesday, 11/18. You can stay as long as you like, but keep in mind
-          all guests are required to a minimum of three nights. We recommend
-          coming at least the day before 11/19, staying for the weekend, and
-          checking out 11/22. There will be a planned included group activity
-          the day after the wedding, Saturday 11/21. Plan to stay at least 3
-          nights in the window November 18th - 22nd
+          The wedding party will arrive Wednesday, 11/18. You can stay as long
+          as you like, but keep in mind all guests are required to a minimum of
+          three nights. We recommend coming at least the day before 11/19,
+          staying for the weekend, and checking out 11/22. There will be a
+          planned included group activity the day after the wedding, Saturday
+          11/21. Plan to stay at least 3 nights in the window November 18th -
+          22nd
         </li>
         <li>
-          <strong>Do not</strong> book a hotel, Airbnb, or any other lodging
-          just yet
-          <ul>
-            <li>We will send you information regarding the room block</li>
-          </ul>
-          <li>You may buy flights now</li>
+          <strong>What room should I get?</strong>
+          <br />
+          This is totally up to you. The wedding guests will be spread out
+          through the resort and all rooms have access to all amenities with the
+          exception of the adults only Turquoise area - is a small rooftop pool
+          area. If you’re into luxury and don’t have kids feel welcome to stay
+          at the Turquoise of course. We - the wedding couple - will likely stay
+          there, but don’t feel bad if you don’t.
+        </li>
+        <li>
+          <strong>Can I invite a plus 1?</strong>
+          <br />
+          Yes, anyone that you love we love and is welcome to come! One of the
+          benefits of a destination wedding is we don’t have to sweat about head
+          count.
         </li>
       </ul>
     </div>
