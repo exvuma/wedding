043db5ea3bef08417082db8b54977ee7888318f4
Victoria <Bernard.victoria22@gmail.com>
2020-03-08 16:46:27 -0500

    booking revision

--- src/components/sections/Book.tsx
+++ src/components/sections/Book.tsx
@@ -3,25 +3,40 @@ export const Book: React.FC = () => (
   <ContainerSmall>
     <div>
       <p>
-        John and Victoria are getting married. We want you there to share our
-        special day more than any gift on a registary. We are get
+        We’re getting married at the beautiful Hyatt Ziva in Cancun Mexico. We
+        want you there to share our special day with us more than any gift on a
+        registry. Book your room on the hotel’s room block we have set up as
+        soon as possible. To work with your budget, the travel agency allows
+        interest free payments.
       </p>
-      We’re getting married at the beautiful Hyatt Ziva in Cancun Mexico. We
-      want you there to share our special day with us more than any gift on a
-      registry. Book your room on the hotel’s room block we have set up as soon
-      as possible. To work with your budget, the travel agency allows interest
-      free payments. To book your hotel reservation start out by visiting our
-      travel agent’s [booking site](). You’ll find a lot more [Book hotel
-      here](https://www.destinationweddings.com/Weddings/Guests/AttendaWedding.aspx?dw_weddingcode=MARRYTHEJOHN&dw_bname=Bernard)
-      Wedding couple’s last name: Fawcett Wedding code: MARRYTHEJOHN Our number
-      one goal is to get you, our guests, there and meet one another. Please
-      don’t hestiate to ask for help for us to get you there. For any inquiries
-      about the room block or transportation contact our amazing travel agent,
-      Jessica Bull. If you get stuck any step of the way aside from room booking
-      contact us. Victoria - (408)296-2467 or John (469) 587-5077 TLDR; Book
-      your hotel book now for at least the minimum days you’d like to stay. You
-      can always add days to your reservations later. Book your flight on your
-      favorite airline (we recommend Southwest ) whatever time works.
+      <p>
+        To book your hotel reservation start out by visiting our travel agent’s
+        <a href="https://www.destinationweddings.com/Weddings/Guests/AttendaWedding.aspx?dw_weddingcode=MARRYTHEJOHN&dw_bname=Bernard">
+          &nbsp;booking site
+        </a>
+        , register an account, and you’ll be directed to fill out a form. You’ll
+        find a lot more details about the resort there.
+      </p>
+      <ul>
+        <li>Wedding couple’s last name: Fawcett</li>
+        <li>Wedding code: MARRYTHEJOHN</li>
+      </ul>
+      <p>
+        Our number one goal is to get you, our guests, there and meet one
+        another. Please don’t hesitate to ask for help for us to get you there.
+        For any inquiries about the room block or transportation contact our
+        amazing travel agent, Jessica Bull. Which you’ll find her contact on the
+        form. If you get stuck any step of the way aside from room booking
+        contact us. Victoria - (408) 396-2467 or John (469) 587-5077
+      </p>
+      <h2>TLDR; </h2>
+      <p>
+        Book your hotel book now for at least the minimum days you’d like to
+        stay. You can always add days to your reservations later. Book your
+        flight on your favorite airline (we recommend Southwest ) whatever time
+        works.
+      </p>
+      <button>BOOK NOW</button>
     </div>
   </ContainerSmall>
 )
