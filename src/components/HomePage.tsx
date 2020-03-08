5c02606b96a509d79f69ef12d6c1f4df49c0ff14
Victoria <Bernard.victoria22@gmail.com>
2020-03-08 17:03:03 -0500

    planes workers

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Box } from 'reflexbox'
+import { Box, Flex } from 'reflexbox'
 import { DefaultLayout } from '../layouts/DefaultLayout'
 import { ContainerSmall, Container } from './Container'
 import { fontFamily, space } from '../theme'
@@ -65,26 +65,40 @@ export const HomePage: React.FC = () => {
             </ContainerSmall>
           </Panel>
           <Panel id="book" setActiveTab={setActiveTab}>
-            <Box mt={space[7] + 'em'} mb={space[4] + 'em'}>
-              <ColoredBlade base="red" index={6}>
-                <div style={{ zIndex: 2 }}>
-                  {/* <div style={{ zIndex: 2, position: 'relative' }}> */}
-                  <Box p={space[4] + 'em'} textAlign="center">
-                    <h2 style={{ fontSize: '3em' }}>Book</h2>
-                  </Box>
-                </div>
-                <Box height={space[7] * 2 + 'em'}>
-                  <PlaneAnimation numPlanes={8} />
+            <ColoredBlade base="blue" index={6}>
+              <Flex>
+                {/* <Flex pt={space[7] + 'em'} pb={space[4] + 'em'}> */}
+                <Box style={{ position: 'relative' }} flex={1}>
+                  <PlaneAnimation numPlanes={4} centerWidth={50} />
                 </Box>
-              </ColoredBlade>
-              <ColoredBlade base="red" index={6}>
-                <Box p={space[4] + 'em'} textAlign="center">
-                  <h2 style={{ fontSize: '3em' }}>
-                    Do NOT Book Off the Room Block!
+                <Box
+                  flex={1}
+                  style={{ position: 'relative', textAlign: 'center' }}
+                >
+                  <PlaneAnimation numPlanes={0} centerWidth={50} />
+                  <h2
+                    style={{
+                      fontSize: '3em',
+                      marginTop: space[3] + 'em',
+                      marginBottom: space[1] + 'em',
+                    }}
+                  >
+                    Book
                   </h2>
                 </Box>
-              </ColoredBlade>
-            </Box>
+                <Box style={{ position: 'relative' }} flex={1}>
+                  blah
+                  <PlaneAnimation numPlanes={4} centerWidth={50} />
+                </Box>
+              </Flex>
+            </ColoredBlade>
+            <ColoredBlade base="red" index={6}>
+              <Box p={space[4] + 'em'} textAlign="center">
+                <h2 style={{ fontSize: '3em' }}>
+                  Do NOT Book Off the Room Block!
+                </h2>
+              </Box>
+            </ColoredBlade>
             <Book />
           </Panel>
           <Panel id="faqs" setActiveTab={setActiveTab}>
