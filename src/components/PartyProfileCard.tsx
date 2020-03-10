d6aa0262e497c2c0671cdde9778a93a239819d5f
Victoria <Bernard.victoria22@gmail.com>
2020-03-10 16:45:55 -0500

    Move Cards into utils

--- src/components/PartyProfileCard.tsx
+++ src/components/PartyProfileCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { space, breakpoints, fontSizes, fontFamily, colors } from '../theme'
+import { space, fontFamily, colors } from '../theme'
 import styled from '@emotion/styled'
-import { Box, Flex } from 'reflexbox'
-import { ColoredBlade } from './ColoredBlade'
-import { Container } from './Container'
+import { Box } from 'reflexbox'
 import { SelectableProfile } from './PartyProfileCard2'
+import { CardWrapper } from './utils/CardWrapper'
+import { CardTitle } from './utils/CardTitle'
 
 export type Profile = {
   name: string
@@ -32,39 +32,6 @@ export const PartyProfileCard: React.FC<PartyProfileCardProps> = props => {
     </CardWrapper>
   )
 }
-
-const CardTitle = styled.h3`
-  font-family: ${fontFamily.sansserif};
-  font-size: 0.8em;
-  letter-spacing: 2.2px;
-  text-transform: uppercase;
-  color: ${colors.gray[4]};
-  font-size: ${fontSizes[2]}em;
-  margin: ${space[1]}em 0 0 0;
-  transition: all 0.3s linear;
-`
-
-const CardWrapper = styled.div`
-  background: white;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23000001' fill-opacity='0.05' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
-  border-radius: 3px;
-  padding: ${space[2]}em;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-  text-align: center;
-  font-family: ${fontFamily.sansserif};
-  cursor: pointer;
-  transform: scale(1);
-  transition: all 0.2s ease-out;
-  border-top: solid 8px ${colors.red[5]};
-  &:hover {
-    transform: scale(1.02);
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-
-    & ${CardTitle} {
-      color: ${colors.red[4]};
-    }
-  }
-`
 
 const CardPhotoFrame = styled<'div', { offset: [number, number] }>('div')`
   position: relative;
