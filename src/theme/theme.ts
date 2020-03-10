25a36d4a288f18742c6d56571f51acb3f1b7a1d4
Victoria <Bernard.victoria22@gmail.com>
2020-03-10 16:40:42 -0500

    theme color derived lum

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,6 +3,7 @@ export const newColors = {
   Info: ' #151e1a', //	 "RacingGreen",
   Success: '#728342', // "Dingley",
   Warning: '#ef720a', // "Christine",
+  LightRed: '#ff959f',
   Danger: '#f44336', // "Pomegranate",
   // main
   LightShades: '#F4F0ED', //light green
@@ -11,57 +12,81 @@ export const newColors = {
   DarkAccent: '#B58C83', //grey
   DarkShades: '#262430',
   VeryDarkShade: '#000000',
+  Grey: '#cdccd6',
 } as const
+function ColorLuminance(hex: string, lum: any) {
+  // validate hex string
+  hex = String(hex).replace(/[^0-9a-f]/gi, '')
+  if (hex.length < 6) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
+  }
+  lum = lum || 0
+
+  // convert to decimal and change luminosity
+  var rgb = '#',
+    part,
+    i
+  for (i = 0; i < 3; i++) {
+    part = parseInt(hex.substr(i * 2, 2), 16)
+    part = Math.round(Math.min(Math.max(0, part + part * lum), 255)).toString(
+      16,
+    )
+    rgb += ('00' + part).substr(part.length)
+  }
+
+  return rgb
+}
+console.log(
+  '    ColorLuminance(newColors.MainColor, 0.99),',
+  ColorLuminance(newColors.MainColor, 0.99),
+)
+
 export const colors = {
   //TODO: work on colors
   red: [
-    newColors.LightShades,
-    newColors.LightAccent,
-    newColors.LightAccent,
-    newColors.Danger,
-    newColors.MainColor,
-    newColors.MainColor, //'#b70315', // More important shade of red
-    newColors.DarkAccent,
-    newColors.DarkAccent,
+    ColorLuminance(newColors.LightRed, 1),
+    ColorLuminance(newColors.LightRed, 0.9),
+    ColorLuminance(newColors.MainColor, 0.3),
+    ColorLuminance(newColors.MainColor, 0),
+    ColorLuminance(newColors.MainColor, -0.2),
+    ColorLuminance(newColors.MainColor, -0.4),
+    ColorLuminance(newColors.MainColor, -0.8),
   ],
   blue: [
-    newColors.LightShades,
-    newColors.LightShades,
-    newColors.DarkAccent,
-    newColors.DarkAccent,
-    newColors.DarkAccent,
-    newColors.DarkAccent,
-    newColors.DarkAccent,
-    newColors.DarkAccent,
+    ColorLuminance(newColors.DarkShades, 0.8),
+    ColorLuminance(newColors.DarkShades, 0.7),
+    ColorLuminance(newColors.DarkShades, 0.3),
+    ColorLuminance(newColors.DarkShades, 0),
+    ColorLuminance(newColors.DarkShades, -0.2),
+    ColorLuminance(newColors.DarkShades, -0.4),
+    ColorLuminance(newColors.DarkShades, -0.8),
   ],
   green: [
-    newColors.LightShades,
-    newColors.LightShades,
-    newColors.Success,
-    newColors.Success,
-    newColors.DarkShades,
-    newColors.DarkShades,
-    newColors.DarkShades,
+    ColorLuminance(newColors.DarkAccent, 0.8),
+    ColorLuminance(newColors.DarkAccent, 0.7),
+    ColorLuminance(newColors.DarkAccent, 0.3),
+    ColorLuminance(newColors.DarkAccent, 0),
+    ColorLuminance(newColors.DarkAccent, -0.2),
+    ColorLuminance(newColors.DarkAccent, -0.4),
+    ColorLuminance(newColors.DarkAccent, -0.8),
   ],
   gray: [
-    newColors.LightShades,
-    newColors.LightShades,
-    newColors.DarkAccent,
-    newColors.DarkAccent,
-    newColors.DarkAccent,
-    newColors.DarkAccent,
-    newColors.DarkAccent,
-    newColors.Info,
+    ColorLuminance(newColors.Grey, 0.8),
+    ColorLuminance(newColors.Grey, 0.7),
+    ColorLuminance(newColors.Grey, 0.3),
+    ColorLuminance(newColors.Grey, 0),
+    ColorLuminance(newColors.Grey, -0.2),
+    ColorLuminance(newColors.Grey, -0.4),
+    ColorLuminance(newColors.Grey, -0.8),
   ],
   gold: [
-    newColors.LightShades,
-    newColors.LightShades,
-    newColors.LightAccent,
-    newColors.LightAccent,
-    newColors.LightAccent,
-    newColors.LightAccent,
-    newColors.LightAccent,
-    newColors.LightAccent,
+    ColorLuminance(newColors.LightAccent, 0.8),
+    ColorLuminance(newColors.LightAccent, 0.7),
+    ColorLuminance(newColors.LightAccent, 0.3),
+    ColorLuminance(newColors.LightAccent, 0),
+    ColorLuminance(newColors.LightAccent, -0.2),
+    ColorLuminance(newColors.LightAccent, -0.4),
+    ColorLuminance(newColors.LightAccent, -0.8),
   ],
 } as const
 // const color = Object.keys(oldColors).map(mainColor=>{
